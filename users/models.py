6bfd5f79630d00cd0bbf746dac149a1287737029
furkansimsekli <simseklifurkan0@gmail.com>
2023-07-07 16:33:22 +0300

    users.model: introduce student and moderator profiles
    ImageField requires Pillow to be installed, that is why
    Pillow=10.0.0 has been added to dependencies.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    level = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+    profile_picture = models.ImageField(default='default_profile_pic.jpg ', upload_to='profile_pictures/')
+
+    def __str__(self):
+        print(self.user.username)
+
+
+class ModeratorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    profile_picture = models.ImageField(default='default_profile_pic.jpg ', upload_to='profile_pictures/')
+
+    def __str__(self):
+        print(self.user.username)
