6e5ac164843cf2d857ab3c4736f8fc7ad3cae85b
furkansimsekli <simseklifurkan0@gmail.com>
2023-07-10 11:51:44 +0300

    users: implement initial forms
    login, logout, register pages have been added

--- speakie/urls.py
+++ speakie/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from users.views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'users/register.html')
+
+    def post(self, request):
+        print(request.POST)
+        return render(request, 'users/register.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'users/login.html')
+
+    def post(self, request):
+        print(request.POST.get('username'))
+        return render(request, 'users/login.html')
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('home')
