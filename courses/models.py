98bbd453f28c9e744de1bd923b045ee51cbddbc8
furkansimsekli <simseklifurkan0@gmail.com>
2023-07-18 13:35:51 +0300

    courses.model: fix duplicate slug bug
    Use pseudo-id for new inputs.

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from unidecode import unidecode
 
 from users.models import User
 
@@ -13,13 +14,20 @@ LEVEL_CHOICES = (
 
 class Course(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, allow_unicode=True)
     description = models.CharField(max_length=255)
     flag_picture = models.ImageField(default='default_flag_pic.jpg', upload_to='flag_pictures/')
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        base_slug = slugify(unidecode(self.title))[:250]  # 250 chars for title, 1 char for '-', and 4 char for number
+        self.slug = base_slug
+        slug_num = 1
+
+        while Course.objects.filter(slug=self.slug).exists():
+            self.slug = f'{base_slug}-{slug_num}'
+            slug_num += 1
+
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -29,7 +37,7 @@ class Course(models.Model):
 class TranslationPractice(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, allow_unicode=True)
     question = models.CharField(max_length=255, default='')
     answer = models.CharField(max_length=255, default='')
     choice_1 = models.CharField(max_length=255, default='')
@@ -39,7 +47,14 @@ class TranslationPractice(models.Model):
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)[:256]
+        base_slug = slugify(unidecode(self.title))[:250]  # 250 chars for title, 1 char for '-', and 4 char for number
+        self.slug = base_slug
+        slug_num = 1
+
+        while TranslationPractice.objects.filter(slug=self.slug).exists():
+            self.slug = f'{base_slug}-{slug_num}'
+            slug_num += 1
+
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -49,13 +64,20 @@ class TranslationPractice(models.Model):
 class SpeakingPractice(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, allow_unicode=True)
     paragraph = models.TextField()
     difficulty = models.SmallIntegerField(default=0, choices=LEVEL_CHOICES)
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        base_slug = slugify(unidecode(self.title))[:250]  # 250 chars for title, 1 char for '-', and 4 char for number
+        self.slug = base_slug
+        slug_num = 1
+
+        while SpeakingPractice.objects.filter(slug=self.slug).exists():
+            self.slug = f'{base_slug}-{slug_num}'
+            slug_num += 1
+
         super().save(*args, **kwargs)
 
     def __str__(self):
