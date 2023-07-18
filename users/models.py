15bc628ab65dc80b7c7aace099bfe94dbd8e4ad8
furkansimsekli <simseklifurkan0@gmail.com>
2023-07-18 16:35:37 +0300

    users.model: add level field
    Levels are being calculated by a dummy formula. Each level contains
    1.5 larger xp than previous level. And first level is 100.

--- users/models.py
+++ users/models.py
@@ -1,18 +1,22 @@
+import math
+
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from PIL import Image
 
 
 class User(AbstractUser):
     bio = models.TextField(default='', max_length=255)
     profile_picture = models.ImageField(default='default_profile_pic.jpg', upload_to='profile_pictures/')
-    score = models.PositiveIntegerField(default=0)
+    score = models.PositiveIntegerField(default=1)
+    level = models.PositiveIntegerField()
     is_moderator = models.BooleanField(default=False)
 
     def __str__(self):
         print(f'{self.username} - Moderator: {self.is_moderator}')
 
     def save(self, *args, **kwargs):
+        self.level = self.calculate_level()
         super().save()
         img = Image.open(self.profile_picture.path)
 
@@ -20,3 +24,6 @@ class User(AbstractUser):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.profile_picture.path)
+
+    def calculate_level(self):
+        return math.ceil(math.log(1 + 0.005 * self.score, 1.5))
