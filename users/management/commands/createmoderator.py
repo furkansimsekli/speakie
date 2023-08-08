b59309d961dc6ddcfe7452fc8f1938db81ddd340
furkansimsekli <simseklifurkan0@gmail.com>
2023-08-08 13:39:11 +0300

    users: add management command to create a moderator

---
+++ users/management/commands/createmoderator.py
@@ -0,0 +1,35 @@
+import sys
+
+from django.contrib.auth.hashers import make_password
+from django.core.management.base import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Creates a moderator with given credentials'
+
+    def handle(self, *args, **options):
+        username = input('Username: ')
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            self.stdout.write('ERROR: Username is already in use!')
+            sys.exit(1)
+
+        email = input('Email: ')
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            self.stdout.write('ERROR: Email is already in use!')
+            sys.exit(1)
+
+        password = input('Password: ')
+
+        if len(password) == 0:
+            self.stdout.write('ERROR: Please choose a password longer than 1 character!')
+            sys.exit(1)
+
+        password = make_password(password)
+        User.objects.create(username=username, email=email, password=password, is_moderator=True)
+        self.stdout.write('Done!')
